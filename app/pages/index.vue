4a5983eb9716308aee43215b2faf1efe09654b8f
Kevin Kwan <kevinkwan@Kevins-MacBook-Pro.local>
2025-07-30 17:20:35 -0400

    add page meta

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,4 +4,12 @@
 
 <script lang="ts" setup>
 definePageMeta({ layout: "default" });
+
+const title = ref("Page Index");
+const description = ref("Page Index Description");
+
+useSeoMeta({
+  title,
+  description: () => `This is ${description.value}`,
+});
 </script>
