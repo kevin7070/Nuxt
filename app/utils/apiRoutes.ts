a24548bf07eb21bbbc3370371ba5285e989bb387
kevin7070 <kevin.7070@gmail.com>
2025-08-01 11:00:17 -0400

    update apiRoutes

--- app/utils/apiRoutes.ts
+++ app/utils/apiRoutes.ts
@@ -1,13 +1,13 @@
 export const apiRoutes = {
   auth: {
-    csrf: '/auth/csrf/',
-    login: '/auth/login/',
-    logout: '/auth/logout/',
-    refresh: '/auth/refresh/'
+    csrf: '/api/v1/auth/csrf/',
+    login: '/api/v1/auth/login/',
+    logout: '/api/v1/auth/logout/',
+    refresh: '/api/v1/auth/refresh/'
   },
   product: {
     categories: {
-      tree: '/products/categories/tree/',
+      tree: '/api/v1/product/categories/tree/',
     },
   },
 }
